684152c141833390d3b85d3ab43b25cb2e0ba0a1
shuaigegangNo1 <1321486316@qq.com>
2018-12-05 10:44:52 +0800

    modify login html

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -10,23 +10,23 @@ import {AuthService} from "../../common/service/AuthService";
   templateUrl: 'login.html'
 })
 export class loginPage{
-  user = { name: '', password: '123456' };
+  user = { name: '', password: '' };
   constructor(public nav: NavController, public auth: AuthService, public alertCtrl: AlertController) { }
   public login() {
-    if(this.user.name===''){
-      this.showError("未输入工号/学号");
+    if(this.user.name===''||this.user.password===''){
+      this.showError("未输入工号/学号或密码");
       return;
     }
     this.auth.login(this.user).subscribe(allowed => {
         if (allowed) {
           this.nav.push(TabsPage);
         } else {
-          this.showError("工号/学号不正确");
+          this.showError("工号/学号或密码不正确");
         }
       },
       error => {
         if (error.status==401){
-          this.showError("工号/学号不正确");
+          this.showError("工号/学号或密码不正确");
         }else{
           this.showError("网络未连接");
         }
